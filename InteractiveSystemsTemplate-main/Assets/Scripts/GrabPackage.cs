9322680ad37d885f30dbfa16a1545781bcdb2207
CamilaDAlm <72498311+CamilaDAlm@users.noreply.github.com>
2023-05-19 14:04:07 +0200

    musica de fondo + recoger

--- InteractiveSystemsTemplate-main/Assets/Scripts/GrabPackage.cs
+++ InteractiveSystemsTemplate-main/Assets/Scripts/GrabPackage.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class GrabPackage : MonoBehaviour
 {
     // Reference to the character camera.
-    [SerializeField]
-    private Camera characterCamera;
+    //[SerializeField]
+    //private Camera characterCamera;
 
     // Reference to the slot for holding picked item.
     [SerializeField]
@@ -16,31 +16,20 @@ public class GrabPackage : MonoBehaviour
     private Transform parent;
 
     // Reference to the currently held item.
-    private PickPackage pickedItem;
+    //private PickPackage pickedItem;
     private List<PickPackage> itemsPickedList = new List<PickPackage>();
 
-
+    //timer
+    private float totalTime = 5.0f;
+    //public float timeLeft = totalTime;
+    float timer = 5.0f;
     // Update is called once per frame
     void Update()
     {
-        // Execute logic only on button pressed
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            // Check if player picked some item already
-            if(itemsPickedList.Count > 0) //(pickedItem)
-            {
-                // If yes, drop picked item
-                //DropItem(pickedItem);
-                DropItem(itemsPickedList[itemsPickedList.Count - 1]);
-                Debug.Log(itemsPickedList.Count);
-            }
-
-        }
-        else
-        {
+            
             // If no, try to pick item in front of the player
             // Create ray from center of the screen
-            var ray = characterCamera.ViewportPointToRay(Vector3.one * 0.5f);
+           /* var ray = characterCamera.ViewportPointToRay(Vector3.one * 0.5f);
             RaycastHit hit;
             // Shot ray to find object to pick
             if (Physics.Raycast(ray, out hit, 1.5f))
@@ -55,9 +44,30 @@ public class GrabPackage : MonoBehaviour
                     //PickItem(pickable);
                     PickItem(pickable);
                 }
+            }*/
+        // Execute logic only on button pressed
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        // Check if player picked some item already
+         if (itemsPickedList.Count > 0) //(pickedItem)
+         {
+            // If yes, wait X seconds and drop
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                //DropItem(pickedItem);
+                DropItem(itemsPickedList[itemsPickedList.Count - 1]);
+                Debug.Log(itemsPickedList.Count);
+                timer = totalTime;
             }
+            
+
         }
-    }
+
+       // }
+        
+      
+        }
 
 
     private void PickItem(PickPackage item)//(PickPackage item)
@@ -67,7 +77,7 @@ public class GrabPackage : MonoBehaviour
         itemsPickedList.Add(item);
         // Disable rigidbody and reset velocities
         item.Rb.isKinematic = true;
-        item.Rb.detectCollisions = false;
+        //item.Rb.detectCollisions = false;
 
         item.Rb.velocity = Vector3.zero;
         item.Rb.angularVelocity = Vector3.zero;
@@ -79,7 +89,7 @@ public class GrabPackage : MonoBehaviour
         item.transform.localEulerAngles = Vector3.zero;
 
        
-        //pickItems.Add(pickedItem);
+        
     }
 
     private void DropItem(PickPackage item)
@@ -88,12 +98,22 @@ public class GrabPackage : MonoBehaviour
         //pickedItem = null;
         // Remove parent
         item.transform.SetParent(parent);
+        // Reset position and rotation
+        //Vector3 newPosition = new Vector3(item.transform.localPosition.x, 1.1f, item.transform.localPosition.z);
+        //item.transform.localPosition = newPosition;
         //Enable rigidbody
-        item.Rb.isKinematic = false;
-        item.Rb.detectCollisions= true;
+        //item.Rb.isKinematic = false;
+        //item.Rb.detectCollisions= true;
         // Add force to throw item a little bit
         //item.Rb.AddForce(item.transform.forward * 2, ForceMode.VelocityChange);
         itemsPickedList.Remove(item);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickpackage"))
+        {
+            PickItem(other.GetComponent<PickPackage>());
+        }
+    }
 }
--- InteractiveSystemsTemplate-main/Assets/Scripts/PickPackage.cs
+++ InteractiveSystemsTemplate-main/Assets/Scripts/PickPackage.cs
@@ -9,16 +9,35 @@ public class PickPackage : MonoBehaviour
     // Reference to the rigidbody
     private Rigidbody rb;
     public Rigidbody Rb => rb;
+
+    private Collider cl;
+    public Collider Cl => cl;
     /// <summary>
     /// Method called on initialization.
     /// </summary>
+    private bool dropdown;
     private void Awake()
     {
         // Get reference to the rigidbody
         rb = GetComponent<Rigidbody>();
         rb.isKinematic = true;
 
-      
+        cl = GetComponent<Collider>();
+        cl.enabled = true;
     }
 
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //dropdown = true;
+            Destroy(cl);
+        }
+    }
+    
+
+    private void OnTriggerExit(Collider other)
+    {
+        _ = gameObject.AddComponent<MeshCollider>();
+    }*/
 }
